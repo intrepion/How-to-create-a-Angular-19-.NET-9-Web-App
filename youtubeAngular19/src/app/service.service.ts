2538a3e29e9447ff8a283747357fb6d1c43b3efc
Oliver Forral <intrepion@gmail.com>
2025-05-10 13:38:57 -0600

    Setup service service.

--- youtubeAngular19/src/app/service.service.ts
+++ youtubeAngular19/src/app/service.service.ts
@@ -1,9 +1,19 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ServiceService {
 
-  constructor() { }
+  private path = environment.apiUrl
+
+  constructor(private httpClient: HttpClient) { }
+
+  getAllPokemonData(): Observable<any> {
+    const header = new HttpHeaders().set('Content-type', 'application/json');
+    return this.httpClient.get(this.path + 'Pokemon/GetPokemon', { headers: header })
+  }
 }
