c0351936c9f06fc521a1fc10dcb54b4769624426
Oliver Forral <intrepion@gmail.com>
2025-05-10 11:32:40 -0600

    Copied pokemon data drag and drop code.

--- youtubeAngular19/src/app/mainpage/mainpage.component.ts
+++ youtubeAngular19/src/app/mainpage/mainpage.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MaterialModule } from '../material/material.module';
+import {DragDropModule} from '@angular/cdk/drag-drop';
+import {CdkDragDrop, CdkDropList, CdkDrag, moveItemInArray} from '@angular/cdk/drag-drop';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-mainpage',
@@ -8,5 +11,31 @@ import { MaterialModule } from '../material/material.module';
   styleUrl: './mainpage.component.css'
 })
 export class MainpageComponent {
+  private _snackBar = inject(MatSnackBar);
+  // this._snackBar.open(message, action);
 
+  lstData: any = []
+
+  constructor(private service: ServiceService) {
+  }
+
+  ngOnInit() {
+    this.getAllPokemonData()
+  }
+
+  getAllPokemonData() {
+    this.service.getAllPokemonData().subscribe({
+      next: (res: any) => {
+        console.log('pokemonObj', res)
+        this.lstData = res
+      },
+      error: (error: any) => {
+        console.log(error)
+      }
+    })
+  }
+
+  drop(event: CdkDragDrop<{name: string}[]>) {
+    moveItemInArray(this.lstData, event.previousIndex, event.currentIndex);
+  }
 }
