f4d71bd591992d6a41eb72feaa30e3ba5ca63c26
Oliver Forral <intrepion@gmail.com>
2025-05-10 00:30:13 -0600

    Imported material snack bar component.

--- youtubeAngular19/src/app/mainpage/mainpage.component.ts
+++ youtubeAngular19/src/app/mainpage/mainpage.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { MaterialModule } from '../material/material.module';
 
 @Component({
   selector: 'app-mainpage',
-  imports: [],
+  imports: [MaterialModule],
   templateUrl: './mainpage.component.html',
   styleUrl: './mainpage.component.css'
 })
