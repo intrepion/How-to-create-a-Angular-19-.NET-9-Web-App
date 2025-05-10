a3b6994cea19a5c439f96913e624a505b229495e
Oliver Forral <intrepion@gmail.com>
2025-05-10 17:54:59 -0600

    Imported MatSnackBarModule.

--- youtubeAngular19/src/app/mainpage/mainpage.component.ts
+++ youtubeAngular19/src/app/mainpage/mainpage.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { MaterialModule } from '../material/material.module';
 
 @Component({
   selector: 'app-mainpage',
-  imports: [],
+  imports: [
+    MaterialModule
+  ],
   templateUrl: './mainpage.component.html',
   styleUrl: './mainpage.component.scss'
 })
